99d7308834b45adebdc0d830f40d965e2c579c6a
Ubuntu <ubuntu@ip-172-31-20-173.eu-west-1.compute.internal>
2018-02-25 16:12:50 +0000

    the app has index.html

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 var http = require('http');
+const fs = require('fs');
+
+const port = 3000;
+const hostname = "merrilmathew.org";
+fs.readFile('index.html',(err,html)=>{
+	if(err){
+		throw err;
+	}
+	else {
+		const server = http.createServer((req,res)=> {
+		res.statusCode = 200;
+		res.setHeader('Content-type', 'text/html');
+		res.write(html);
+		res.end();
+
+		});
+
+		server.listen(port,hostname,()=> {
+			console.log('Server started on port ' +port);
+		})
+	}
 var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 
 var tone_analyzer = new ToneAnalyzerV3({
@@ -25,3 +46,6 @@ tone_analyzer.tone(params,function(error, response){
 		console.log(JSON.stringify(response, null, 2));
 
 });
+});
+
+
